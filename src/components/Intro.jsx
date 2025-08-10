04e5ea3a35d7ef76d985dbdee6a1abd9bcc05d1b
Charles <chazylites@gmail.com>
2025-08-10 20:46:37 +0100

    added h to 1000w to make it 1000wh

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,7 @@ const Intro = () => (
   <div className="flex flex-col items-center justify-center text-center mt-24 px-4">
   <p className="text-gray-300 text-lg mt-4 mb-4">Introducing</p>
   <h3 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] leading-tight">
-    itel 500w/1000w Power Tank
+    itel 500w/1000wh Power Tank
   </h3>
 </div>
 
